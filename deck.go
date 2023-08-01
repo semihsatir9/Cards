5f514a0a3b22edf9f946e3db043bc2eb74f9b6fb
Semih Satır <semihsatir9@hotmail.com>
2023-08-01 17:56:55 +0300

    Working with Data Types and := element

---
+++ deck.go
@@ -0,0 +1,9 @@
+package main
+
+
+// Custom Types
+// In Go, there is no object oriented classes.
+// In this case, we extend a custom data type from 
+// an already existing type.
+
+type deck []string
--- main.go
+++ main.go
@@ -6,17 +6,48 @@ import "fmt"
 // function but they CANNOT be given value outside
 
 func main() {
-	//var card string = "Ace of Spades"
-	card := newCard()
+	// Declaring Variables
+
+
+	// var card string = "Ace of Spades"
+	
+	// card := newCard()
 
 	// Using := makes Go automatically assign the 
 	// type of the variable for the given data value
 
 	// Only use it when you are defining the data
 	// for the first time!
+	// card = "Five of Diamonds"
+	// For changing the data after declaration
 
-	card = "Five of Diamonds"
-	fmt.Println(card)
+
+	// Slices
+
+	// Like arraylists on Java. Many functions to be used.
+	// Must declare a type for the slice. The slice
+	// will only take data of that type.
+
+	//Append is .add of arraylists
+
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+
+	// For loop
+	
+	// Iterating over a slice, use range to define
+	// the size of the slice.
+
+	// Use := because after every iteration, the old
+	// data is thrown away. You have to assign new values
+	// every time the loop iterates.
+	
+	for i , card := range cards{
+		fmt.Println(i,card) 
+	}
+	
+
 }
 
 func  newCard() string{
